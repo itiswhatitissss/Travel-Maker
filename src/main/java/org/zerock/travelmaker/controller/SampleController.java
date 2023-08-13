f435efebc80cac6cf23535040a90ccadf7933db9
yeeun-noh <gkdms8517@naver.com>
2023-08-14 00:40:09 +0900

    html added

--- src/main/java/org/zerock/travelmaker/controller/SampleController.java
+++ src/main/java/org/zerock/travelmaker/controller/SampleController.java
@@ -10,28 +10,21 @@ import org.zerock.travelmaker.mapper.SampleMapper;
 
 @Controller
 @Log4j2
-@RequestMapping("/sample")
+@RequestMapping("/travelmaker")
 @RequiredArgsConstructor
 public class SampleController {
     @Autowired
     private SampleMapper sampleMapper;
 
-    @GetMapping("/home")
-    public void home() {
+    @GetMapping("/loginSuccess") //로그인이 성공했을때 넘어가는 페이지
+    public void loginSuccess() {
     }
 
-    @GetMapping("/main")
-    public void main() {
+    @GetMapping("/scheduler")
+    public void scheduler() {
     }
 
-    @GetMapping("/login")
-    public void login() {}
 
-    @GetMapping("/boardWriteForm")
-    public void boardWriteForm() {}
-
-    @GetMapping("/friendSearchForm")
-    public void friendSearchForm() {}
 }
 
 
