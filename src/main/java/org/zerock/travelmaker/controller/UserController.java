d7396218e9b24b7eafe7d08e02e4eed9aa22d691
yeeun-noh <gkdms8517@naver.com>
2023-08-17 17:45:52 +0900

    login page added

--- src/main/java/org/zerock/travelmaker/controller/UserController.java
+++ src/main/java/org/zerock/travelmaker/controller/UserController.java
@@ -5,15 +5,17 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.travelmaker.mapper.SampleMapper;
 
 @Controller
 @Log4j2
-@RequestMapping("/main")
+@RequestMapping("/travelmaker")
 @RequiredArgsConstructor
-public class mainController {
+public class UserController {
 
-    @GetMapping("/list")
-    public void mainList(){
-        //예은아 여기
+    @GetMapping("/login") //로그인페이지
+    public void login() {
     }
+
+
 }
