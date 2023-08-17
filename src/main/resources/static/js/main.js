eb5fddc2870f42c931bcb08024edb7b270f08aa0
YOUR <anna27lhl@naver.com>
2023-08-17 23:36:15 +0900

    챗gpt짱

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const itemList = document.querySelectorAll(".item-list li");
+
+    itemList.forEach(item => {
+        item.addEventListener("click", function() {
+            const itemId = this.getAttribute("data-id");
+            loadItemDetails(itemId);
+        });
+    });
+
+    function loadItemDetails(itemId) {
+        const rightContent = document.getElementById("right-content");
+        // Assuming you have a server endpoint to fetch item details
+        const url = `/get-item-details/${itemId}`;
+
+        // Using fetch to make an AJAX request
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // Assuming data contains image URL
+                const imageUrl = data.imageUrl;
+
+                // Creating an image element and appending it to the right content
+                const imageElement = document.createElement("img");
+                imageElement.src = imageUrl;
+                rightContent.innerHTML = "";
+                rightContent.appendChild(imageElement);
+            })
+            .catch(error => {
+                console.error("Error fetching item details:", error);
+            });
+    }
+});
