34c5979e157a9ff399a174ea30c7d92c286af4bf
itiswhatitissss <pkmm1008@naver.com>
2023-08-25 13:01:09 +0900

    planDetail로 가보자

--- src/main/java/org/zerock/travelmaker/controller/PlanController.java
+++ src/main/java/org/zerock/travelmaker/controller/PlanController.java
@@ -3,8 +3,10 @@ package org.zerock.travelmaker.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Log4j2
@@ -13,5 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PlanController {
 
     @GetMapping("/planDetail")
-    public void planDetailGET() {};
+    public void planDetailGET(@RequestParam("plno") Long plno, Model model) {
+        log.info("plno ==================================> : " + plno);
+    };
 }
