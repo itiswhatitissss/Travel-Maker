7273073e4a6168b2bf61be3d9659071ccc0d94d2
yeeun-noh <gkdms8517@naver.com>
2023-08-25 12:18:10 +0900

    plandetail

---
+++ src/main/java/org/zerock/travelmaker/controller/PlanController.java
@@ -0,0 +1,17 @@
+package org.zerock.travelmaker.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/travelmaker/plan/*")
+@RequiredArgsConstructor
+public class PlanController {
+
+    @GetMapping("/planDetail")
+    public void planDetailGET() {};
+}
