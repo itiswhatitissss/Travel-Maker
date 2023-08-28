c3249fa0f42d4520f5f8116ea7ceb7ca7bebec12
leehal <annaleehalim@gmail.com>
2023-08-28 09:50:08 +0900

    친구 구리게 끝ㅊ

--- src/main/java/org/zerock/travelmaker/controller/PlanController.java
+++ src/main/java/org/zerock/travelmaker/controller/PlanController.java
@@ -2,20 +2,60 @@ package org.zerock.travelmaker.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.travelmaker.domain.Users;
+import org.zerock.travelmaker.service.FriendService;
+import org.zerock.travelmaker.service.UserService;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @Log4j2
 @RequestMapping("/travelmaker/plan/*")
 @RequiredArgsConstructor
 public class PlanController {
+    private final FriendService friendService;
+    private final UserService loginService;
 
     @GetMapping("/planDetail")
-    public void planDetailGET(@RequestParam("plno") Long plno, Model model) {
+    public void planDetailGET(@RequestParam("plno") Long plno, Model model, Authentication authentication, HttpSession session) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+
+        Long uno = loginService.getUno(username);
+
         log.info("plno ==================================> : " + plno);
+
+        List<Map<String, Object>> friendDTO = (List<Map<String, Object>>) session.getAttribute("friendSearchResult");
+        if (friendDTO == null) {
+            List<Map<String,Object>> friend =friendService.friendList(uno);
+            model.addAttribute("friendDTO",friend);
+        } else {
+            for(int i=0;i<friendDTO.size();i++) {
+                Map<String, Object> userMap = friendDTO.get(0); // 첫 번째 검색 결과 사용
+                // 필드 값 추출
+                Long fno = (Long) userMap.get("uno");
+                String name = (String) userMap.get("name");
+                String id = (String) userMap.get("id");
+                Users users = Users.builder()
+                        .id(id)
+                        .uno(fno)
+                        .name(name)
+                        .build();
+                model.addAttribute("friendSearchResult", users);
+//            model.addAttribute("list",1);
+            }
+            List<Map<String, Object>> friend = friendService.friendList(uno);
+            model.addAttribute("friendDTO", friend);
+            session.removeAttribute("friendSearchResult"); // 세션에서 검색 결과 제거
+        }
     };
 }
