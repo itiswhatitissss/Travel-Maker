3660c3fb3a3468c22f117aae4e2500b9c5831155
itiswhatitissss <pkmm1008@naver.com>
2023-08-28 10:47:50 +0900

    예은아 받아라

---
+++ src/main/java/org/zerock/travelmaker/domain/PersistentLogins.java
@@ -0,0 +1,17 @@
+package org.zerock.travelmaker.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class PersistentLogins {
+
+    @Id
+    private String series;
+
+    private String username;
+    private String token;
+    private LocalDateTime lastUsed;
+
+}
